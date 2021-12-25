9563e7b5909f8744624edbe54e3f3689fa5bca35
arxxm <7767995@gmail.com>
2021-12-25 21:09:26 +0600

    car

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"func/file"
-)
-
 func main() {
-	emptyFile, err := file.New("/tmp/empty.txt")
-	if err != nil {
-		panic(err)
-	}
+	// emptyFile, err := file.New("/tmp/empty.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fillerFile, err := file.New("/tmp/file.txt", file.UID(1000), file.Contents("My text for file"))
-	if err != nil {
-		panic(err)
-	}
+	// fillerFile, err := file.New("/tmp/file.txt", file.UID(1000), file.Contents("My text for file"))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println(emptyFile, fillerFile)
+	// fmt.Println(emptyFile, fillerFile)
+
+	assembly := car.NewBuilder().Color(car.RedColor)
+
+	familyCar := assembly.Wheels(car.SportsWheels).TopSpeed(50 * car.MPH).Build()
+	familyCar.Drive()
+
+	sportsCar := assembly.Wheels(car.SteelWheels).TopSpeed(150 * car.MPH).Build()
+	sportsCar.Drive()
 }
